2fdf2a018d36edc8f3b4c0ae41f3cc0aa3918ad1
yaosipau <ewgus92@yandex.ru>
2019-03-26 09:32:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ $('.navbar a').on('click', function(e) {
 		},800);		
 	}
 });
+
+// var modal = document.getElementById('mymodal');
+// var img = document.getElementById('myimg');
+// var modalImg = document.getElementById('img01');
+// var captionText = document.getElementById('caption');
+
+// img.onclick = function() {
+// 	modal.style.display = "block";
+// 	modalImg.src = this.src;
+// }
+$('img').on('click', function() {
+	$('.modal').css({'display':'block'});
+	var imgsrc = $(this).attr('src');
+	$('.modal-content').attr('src',imgsrc);
+
+})
+$('.close').on('click', function() {
+	$('.modal').css({'display':'none'});
+})
+// var span = document.getElementsByClassName('close')[0];
+// span.onclick = function() {
+// 	modal.style.display='none';
+// }
